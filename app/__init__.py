9e1f38eb28b3713464fd0eb67d25d2758ee1b8ac
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2019-06-18 18:45:37 +0700

    template sceleton

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_babel import Babel
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+
+bootstrap = Bootstrap()
+babel = Babel()
+login = LoginManager()
 
 
 def create_app():
     app = Flask(__name__)
+    login.init_app(app)
+    bootstrap.init_app(app)
+    babel.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
-from app.main import bp
+from flask import render_template
 
+from app.main import bp
+#from flask_login import current_user
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return "Hello, World, Comrade!"
+    return render_template('index.html')
