b69043be94c56aad16effc918635f646efe53826
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2019-06-19 11:04:16 +0700

    login page and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
 from flask import Flask
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
-
+db = SQLAlchemy()
+migrate = Migrate()
 bootstrap = Bootstrap()
 babel = Babel()
 login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     login.init_app(app)
     bootstrap.init_app(app)
     babel.init_app(app)
@@ -18,5 +29,11 @@ def create_app():
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
     return app
+
+
+from app import models
 
--- app/main/routes.py
+++ app/main/routes.py
@@ -3,6 +3,7 @@ from flask import render_template
 from app.main import bp
 #from flask_login import current_user
 
+
 @bp.route('/')
 @bp.route('/index')
 def index():
