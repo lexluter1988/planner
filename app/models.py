c08ea7b1db554e9f67c94730d297d6d41e5655cc
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2019-06-21 22:00:26 +0700

    blank task model added

--- app/models.py
+++ app/models.py
@@ -45,6 +45,34 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    description = db.Column(db.Text)
+
+    # TODO: this is reference to Milestones, and to Project but could be none.
+    project = db.Column(db.Integer, nullable=True)
+    milestone = db.Column(db.Integer, nullable=True)
+    # TODO: implement choices
+    priority = db.Column(db.String(64))
+    # TODO: this is reference to Kovi type of view
+    group = db.Column(db.String(64))
+
+    # TODO: implement choices, also used for factory view and kanban view
+    status = db.Column(db.String(64))
+    created = db.Column(db.DateTime)
+    ended = db.Column(db.DateTime)
+    deadline = db.Column(db.DateTime)
+
+    schedule = db.Column(db.DateTime)
+    estimated = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
+
+    actions = db.Column(db.String(120))
+
+
 @login.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
--- planner.py
+++ planner.py
@@ -1,10 +1,10 @@
 from app import create_app, db
-from app.models import User
+from app.models import User, Task, Note
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Note': Note, 'Task': Task}
 
