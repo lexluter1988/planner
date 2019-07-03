72a16769a31c3ace710a0a2a62e6558a405e74e5
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2019-07-03 19:34:28 +0700

    implemented delete with get for notes

--- app/notes/routes.py
+++ app/notes/routes.py
@@ -24,3 +24,16 @@ def add():
         return redirect(url_for('notes.add'))
     return render_template('notes/notes.html', form=form, notes=notes)
 
+
+# TODO: implement normal js delete method
+@bp.route('/notes/<int:note_id>', methods=['GET'])
+@login_required
+def delete(note_id):
+    note = Note.query.filter_by(id=note_id).first_or_404()
+    if note:
+        db.session.delete(note)
+        db.session.commit()
+        flash(_('Your note is deleted'))
+    else:
+        flash(_('No note with such id found'))
+    return redirect(url_for('notes.add'))
